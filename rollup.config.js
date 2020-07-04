362514e41032949bd10ff70ea752f48df29a3a77
pngwn <hello@pngwn.io>
2020-07-04 10:18:41 +0100

    add simple cjs rollup build

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import cjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+    input: 'index.js',
+    plugins: [
+        cjs(),
+        resolve()
+    ],
+    output: {
+        file: 'dist/index.js',
+        format: 'cjs'
+    }
+}
